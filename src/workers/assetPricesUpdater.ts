3bae45033c4159017e296041995ff462e2bea0f5
Nebule <horseheadb33@proton.me>
2024-06-24 18:34:54 +0200

    fix uuid behavior

--- src/workers/assetPricesUpdater.ts
+++ src/workers/assetPricesUpdater.ts
@@ -1,4 +1,3 @@
-import { EventEmitter } from 'events';
 import { parentPort } from 'worker_threads';
 import Fmp from '../utils/fmpUtils';
 import { WorkerMessage } from '../types/worker';
@@ -11,10 +10,64 @@ import {
 import { AssetVariable, Assets } from '../types/assets';
 import { checkAsset, updateAssets } from '../services/assetsService';
 
-const eventEmitter = new EventEmitter();
-
 const assetPairPriceCollection: { [uuid: string]: PairPrice } = {};
 const requestLocks: { [uuid: string]: string } = {};
+
+async function getPP(
+  pAssetA: string,
+  pAssetB: string,
+  abPrecision: number,
+  confPrecision: number,
+  maxTimestampDiff: number,
+): Promise<PairPrice | null> {
+  const assetA: AssetVariable | null = formatAsset(pAssetA);
+  const assetB: AssetVariable | null = formatAsset(pAssetB);
+
+  if (!assetA || !checkAsset(assetA.prefix, assetA.assetName)) {
+    return null;
+  }
+
+  if (!assetB || !checkAsset(assetB.prefix, assetB.assetName)) {
+    return null;
+  }
+
+  let assetPrices: Prices = {};
+
+  let fmpRes: RetrievedPrices | null = null;
+  let retryCount = 0;
+
+  while (!fmpRes && retryCount < 3) {
+    fmpRes = await Fmp.getLatestPrices([assetA, assetB]);
+    retryCount++;
+  }
+
+  if (!fmpRes) {
+    return null;
+  }
+
+  assetPrices = updateAssetPrices(assetPrices, fmpRes);
+
+  const a = assetPrices[assetA.prefix + '.' + assetA.assetName];
+  const b = assetPrices[assetB.prefix + '.' + assetB.assetName];
+
+  if (!a || !b) {
+    return null;
+  }
+
+  const pairPrice = getPairPrice(
+    a,
+    b,
+    abPrecision,
+    confPrecision,
+    maxTimestampDiff,
+  );
+
+  if (!pairPrice) {
+    return null;
+  }
+
+  return pairPrice;
+}
 
 async function waitForUnlockOrCancel(uuid: string): Promise<void> {
   return new Promise((resolve) => {
@@ -59,21 +112,33 @@ parentPort?.on('message', async (message: WorkerMessage) => {
       });
     }
   } else if (message.type === 'getpairprice') {
-    if (message.payload.uuid && !requestLocks[message.payload.uuid]) {
-      requestLocks[message.payload.uuid] === 'locked';
-      setTimeout(() => {
-        delete requestLocks[message.payload.uuid];
-        delete assetPairPriceCollection[message.payload.uuid];
-      }, 30000);
-    } else if (
-      message.payload.uuid &&
-      requestLocks[message.payload.uuid] &&
-      requestLocks[message.payload.uuid] === 'locked'
-    ) {
-      await waitForUnlockOrCancel(message.payload.uuid);
+    if (message.payload.uuid) {
+      if (!requestLocks[message.payload.uuid]) {
+        requestLocks[message.payload.uuid] === 'locked';
+        const pp = await getPP(
+          message.payload.assetA,
+          message.payload.assetB,
+          message.payload.abPrecision,
+          message.payload.confPrecision,
+          message.payload.maxTimestampDiff,
+        );
+        if (!pp) {
+          requestLocks[message.payload.uuid] = 'canceled';
+        } else {
+          assetPairPriceCollection[message.payload.uuid] = pp;
+          requestLocks[message.payload.uuid] = 'unlocked';
+        }
+        setTimeout(() => {
+          delete requestLocks[message.payload.uuid];
+          delete assetPairPriceCollection[message.payload.uuid];
+        }, 30000);
+      } else if (requestLocks[message.payload.uuid] === 'locked') {
+        await waitForUnlockOrCancel(message.payload.uuid);
+      }
       if (
         !requestLocks[message.payload.uuid] ||
-        requestLocks[message.payload.uuid] === 'canceled'
+        requestLocks[message.payload.uuid] === 'canceled' ||
+        !assetPairPriceCollection[message.payload.uuid]
       ) {
         parentPort?.postMessage({
           uuid: message.uuid,
@@ -90,88 +155,18 @@ parentPort?.on('message', async (message: WorkerMessage) => {
       return;
     }
 
-    const assetA: AssetVariable | null = formatAsset(message.payload.assetA);
-    const assetB: AssetVariable | null = formatAsset(message.payload.assetB);
-
-    if (!assetA || !checkAsset(assetA.prefix, assetA.assetName)) {
-      if (message.payload.uuid && requestLocks[message.payload.uuid]) {
-        requestLocks[message.payload.uuid] = 'canceled';
-      }
-      parentPort?.postMessage({
-        uuid: message.uuid,
-        type: 'resultError',
-        payload: `${message.payload.assetA} doesn't exists`,
-      });
-      return;
-    }
-
-    if (!assetB || !checkAsset(assetB.prefix, assetB.assetName)) {
-      if (message.payload.uuid && requestLocks[message.payload.uuid]) {
-        requestLocks[message.payload.uuid] = 'canceled';
-      }
-      parentPort?.postMessage({
-        uuid: message.uuid,
-        type: 'resultError',
-        payload: `${message.payload.assetB} doesn't exists`,
-      });
-      return;
-    }
-
-    let assetPrices: Prices = {};
-
-    let fmpRes: RetrievedPrices | null = null;
-    let retryCount = 0;
-
-    while (!fmpRes && retryCount < 3) {
-      fmpRes = await Fmp.getLatestPrices([assetA, assetB]);
-      retryCount++;
-    }
-
-    if (!fmpRes) {
-      if (message.payload.uuid && requestLocks[message.payload.uuid]) {
-        requestLocks[message.payload.uuid] = 'canceled';
-      }
-      parentPort?.postMessage({
-        uuid: message.uuid,
-        type: 'resultError',
-        payload: 'Unable to fetch prices',
-      });
-      return;
-    }
-
-    assetPrices = updateAssetPrices(assetPrices, fmpRes);
-
-    const a = assetPrices[assetA.prefix + '.' + assetA.assetName];
-    const b = assetPrices[assetB.prefix + '.' + assetB.assetName];
-
-    if (!a || !b) {
-      if (message.payload.uuid && requestLocks[message.payload.uuid]) {
-        requestLocks[message.payload.uuid] = 'canceled';
-      }
-      parentPort?.postMessage({
-        uuid: message.uuid,
-        type: 'resultError',
-        payload: 'Unable to get pair price',
-      });
-      return;
-    }
-
-    const pairPrice = getPairPrice(
-      a,
-      b,
+    const pp = await getPP(
+      message.payload.assetA,
+      message.payload.assetB,
       message.payload.abPrecision,
       message.payload.confPrecision,
       message.payload.maxTimestampDiff,
     );
-
-    if (!pairPrice) {
-      if (message.payload.uuid && requestLocks[message.payload.uuid]) {
-        requestLocks[message.payload.uuid] = 'canceled';
-      }
+    if (!pp) {
       parentPort?.postMessage({
         uuid: message.uuid,
         type: 'resultError',
-        payload: 'Unable to get pair price',
+        payload: `Unable to fetch prices`,
       });
       return;
     }
@@ -179,12 +174,7 @@ parentPort?.on('message', async (message: WorkerMessage) => {
     parentPort?.postMessage({
       uuid: message.uuid,
       type: message.type,
-      payload: pairPrice,
+      payload: pp,
     });
-
-    if (message.payload.uuid && requestLocks[message.payload.uuid]) {
-      assetPairPriceCollection[message.payload.uuid] = pairPrice;
-      requestLocks[message.payload.uuid] = 'unlocked';
-    }
   }
 });
